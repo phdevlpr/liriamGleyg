d3b22db7d8e08d4203fbd7b572cebc8b69787f7b
phdevlpr <phdevlpr@gmail.com>
2022-07-06 14:35:34 -0300

    Correções  e menu mobile

--- script.js
+++ script.js
@@ -18,3 +18,27 @@ animeScroll();
 window.addEventListener("scroll", () => {
     animeScroll();
 });
+
+
+
+//TOGGLE MENU//
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault();
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+    const active = nav.classList.contains('active');
+    event.curretTarget.setAttribute('aria-expanded', 'active');
+    if (active) {
+        event.currentTarget.setAttribute('aria-label', 'Close menu');
+    } else {
+        event.currentTarget.setAttribute('aria-label', 'Open menu');
+    }
+    
+}
+
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
