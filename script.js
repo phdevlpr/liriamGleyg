87a0833b1fe9c2525b1c27e110ad9d367df1069c
phdevlpr <phdevlpr@gmail.com>
2023-04-13 16:45:07 -0300

    Correções e reestilização

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const item = document.querySelectorAll("[data-anime]");
 
 const animeScroll = () => {
 
-    const windowTop = window.pageYOffset + ((window.innerHeight * 0.95));
+    const windowTop = window.pageYOffset + ((window.innerHeight * 0.85));
     item.forEach((element) => {
         if (windowTop > element.offsetTop) {
             element.classList.add("animate");
